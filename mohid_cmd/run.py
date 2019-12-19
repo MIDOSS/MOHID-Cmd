9ed60313e12729baca4d3dcceb3a5bd3e9878fa0
Rachael D. Mueller <59846131+RachaelDMueller@users.noreply.github.com>
2019-12-19 12:10:00 -0500

    mohid_cmd/run.py: Increased memory to 14.5 MB

--- mohid_cmd/run.py
+++ mohid_cmd/run.py
@@ -227,7 +227,7 @@ def _sbatch_directives(run_desc, results_dir):
         #SBATCH --mail-user={email}
         #SBATCH --mail-type=ALL
         #SBATCH --cpus-per-task=1
-        #SBATCH --mem-per-cpu=14100m
+        #SBATCH --mem-per-cpu=14500m
         #SBATCH --time={walltime}
         #SBATCH --output={results_dir/'stdout'}
         #SBATCH --error={results_dir/'stderr'}
